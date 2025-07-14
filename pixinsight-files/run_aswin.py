921565fe27b0006b074d3e47fa5bb836af022f86
Артур <laokrit@gmail.com>
2025-07-14 23:32:50 +0300

    update inference script to work with linear data

--- pixinsight-files/run_aswin.py
+++ pixinsight-files/run_aswin.py
@@ -6,9 +6,10 @@ from cv2 import imread, imwrite, IMREAD_UNCHANGED, IMWRITE_TIFF_COMPRESSION
 from cv2.typing import MatLike
 from gc import collect
 from transformers import Swin2SRImageProcessor
-from typing import Optional
 from logging import basicConfig, getLogger, StreamHandler, INFO
 
+
+T = -0.51082562376  # ln(0.6)
 
 basicConfig(level=INFO)
 logger = getLogger('astro-swin')
@@ -16,67 +17,48 @@ handler = StreamHandler(sys.stdout)
 logger.addHandler(handler)
 
 
-def auto_stretch(v: np.ndarray, eps: float = 1e-2, g: float = 1.1):
-    scale = (1 + eps) / (v + eps)
-    v_scaled = (v * scale) ** g
-    return v_scaled, scale, g
+def autostretch(image: np.ndarray, eps: float = 1e-2):
+    im_min, im_max = image.min(), image.max()
+    min_max_scaled = (image - im_min) / (im_max - im_min)
+    mean = min_max_scaled.mean()
+    mean_scaled = ((1+eps)/(mean+eps)) * mean
+    best_gamma =  T / np.log(mean_scaled)
+    scale = (1 + eps) / (min_max_scaled + eps)
+    scaled_image = (min_max_scaled * scale) ** best_gamma
+    return {
+        'img': scaled_image,
+        'scale': scale,
+        'gamma': best_gamma,
+        'im_max': im_max,
+        'im_min': im_min,
+    }
 
-def unstretch(v: np.ndarray, scale: np.ndarray, g: float) -> np.ndarray:
-    return (v ** (1/g)) / scale
+def unstretch(img: np.ndarray, scale: np.ndarray, gamma: float, im_max: float, im_min: float) -> np.ndarray:
+    return ((img ** (1/gamma)) / scale) * (im_max - im_min) + im_min
 
-def to_pil(v: np.ndarray):
-    v = np.clip(v, 0, 1)
-    v = np.moveaxis(v, source=0, destination=-1)
-    return v.astype(np.float32)
+def to_pil(img: np.ndarray):
+    img = np.clip(img, 0, 1)
+    img = np.moveaxis(img, source=0, destination=-1)
+    return img.astype(np.float32)
 
 def get_pad_based_size(size: int, window: int, pad: int):
     return (size // (window - pad) + int(size % (window - pad) != 0)) * (window - pad) + pad
 
-def calculate_patch_weights(image, patch_size=32, beta=0.5):
-    c, h, w = image.shape
-    h = (h // patch_size + int(h % patch_size != 0)) * patch_size
-    w = (w // patch_size + int(w % patch_size != 0)) * patch_size
-    canvas = np.zeros((c, h, w))
-    canvas[:, :image.shape[-2], :image.shape[-1]] += image
-    canvas = canvas.mean(axis=0)
-
-    h_patches = h // patch_size
-    w_patches = w // patch_size
-    patches = canvas.reshape(h_patches, patch_size, w_patches, patch_size)
-    patches = patches.transpose(0, 2, 1, 3)      # (h_p, w_p, ps, ps)
-
-    means = np.mean(patches, axis=(-2, -1))      # (h_p, w_p)
-    variances = np.var(patches, axis=(-2, -1))   # (h_p, w_p)
-
-    means_norm = (means - np.min(means) + 1e-8) / (np.max(means) - np.min(means) + 1e-8)
-    variances_norm = (variances - np.min(variances) + 1e-8) / (np.max(variances) - np.min(variances) + 1e-8)
-
-    weights = ((1 + beta**2) * means_norm * variances_norm) / (means_norm * (beta ** 2) + variances_norm)
-
-    weights = weights.repeat(patch_size, axis=0).repeat(patch_size, axis=1)
-    fit_weights = np.expand_dims(weights[:image.shape[-2], :image.shape[-1]], 0)
-    return np.repeat(fit_weights / fit_weights.max(), 3, 0)
-
 def create_weight_mask(size, overlap):
     mask = np.ones((1, 1, size, size))
     fade = np.linspace(0, 1, overlap)
-
+    # horizontal
     mask[..., :overlap, :] *= fade.reshape(1, 1, -1, 1)
     mask[..., -overlap:, :] *= np.flip(fade, axis=0).reshape(1, 1, -1, 1)
-
+    # vertical
     mask[..., :, :overlap] *= fade.reshape(1, 1, 1, -1)
     mask[..., :, -overlap:] *= np.flip(fade, axis=0).reshape(1, 1, 1, -1)
-
     return mask
 
 def terminate_blur(
     image: MatLike,
     session: ort.InferenceSession,
     processor: Swin2SRImageProcessor,
-    mask_patch_size: Optional[int] = None,
-    mask_beta: Optional[float] = None,
-    mask_const: Optional[float] = None,
-    mask_multiplier: Optional[float] = None,
 ):
     window = 256
     pad = 32
@@ -105,21 +87,12 @@ def terminate_blur(
         progress = x / pad_based_width
         logger.info(f'{progress * 100:0.2f}%:\t[{"|" * int(progress * 10) + " " * int((1 - progress) * 10)}]')
     target /= np.clip(weight_sum, a_min=1e-6, a_max=weight_sum.max())
-    processed, base_image = target[0][:, :image.shape[0], :image.shape[1]], img_tensor[0][:, :image.shape[0], :image.shape[1]]
-    if mask_patch_size is None or mask_beta is None or mask_const is None or mask_multiplier is None:
-        return to_pil(processed)
-    patch_weights = calculate_patch_weights(base_image, patch_size=int(mask_patch_size), beta=float(mask_beta))
-    rescaled_weights = np.clip((patch_weights + float(mask_const)) * float(mask_multiplier), a_max=1, a_min=0)
-    return to_pil(rescaled_weights * processed + (1 - rescaled_weights) * base_image)
+    return to_pil(target[0][:, :image.shape[0], :image.shape[1]])
 
 def process(
     model_path: str,
     image_input_path: str,
     image_output_path: str,
-    mask_patch_size: Optional[int] = None,
-    mask_beta: Optional[float] = None,
-    mask_const: Optional[float] = None,
-    mask_multiplier: Optional[float] = None
 ):
     options = ort.SessionOptions()
     options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
@@ -135,18 +108,10 @@ def process(
     sess = ort.InferenceSession(model_path + '/model.onnx', sess_options=options, providers=providers)
     processor = Swin2SRImageProcessor.from_pretrained(model_path)
     image = imread(image_input_path, IMREAD_UNCHANGED)
-    im_stretched, scale, g = auto_stretch(image)
+    stretch_result = autostretch(image)
 
-    processed_tensor = terminate_blur(
-        im_stretched,
-        sess,
-        processor,
-        mask_patch_size=mask_patch_size,
-        mask_beta=mask_beta,
-        mask_const=mask_const,
-        mask_multiplier=mask_multiplier
-    )
-    linear_tensor = unstretch(processed_tensor, scale, g)
+    stretch_result['img'] = terminate_blur(stretch_result['img'], sess, processor)
+    linear_tensor = unstretch(**stretch_result)
     imwrite(image_output_path, linear_tensor, [IMWRITE_TIFF_COMPRESSION, 0])
 
 def parse_args(arg_list: list[str]):
@@ -156,10 +121,6 @@ def parse_args(arg_list: list[str]):
         '-i': 'image_input_path',
         '-o': 'image_output_path',
         '-m': 'model_path',
-        '--patch-size': 'mask_patch_size',
-        '--beta': 'mask_beta',
-        '--const': 'mask_const',
-        '--mul': 'mask_multiplier',
     }
     result = {}
     for i in range(len(arg_list) // 2):
